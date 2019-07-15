9c0fe1118ece111e6163b5c17b23be7ddb6db27b
kentv0 <k3nt0154@gmail.com>
2019-07-14 20:20:32 -0700

    Updates

---
+++ precache-manifest.01874efcf69901bbe571f9d251e78924.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e3cde0564508812308baa2211354a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64d348ada857db226fc6",
+    "url": "/static/css/main.b46a1727.chunk.css"
+  },
+  {
+    "revision": "844dec2cadcc0c24cd61",
+    "url": "/static/js/2.6a9fa077.chunk.js"
+  },
+  {
+    "revision": "64d348ada857db226fc6",
+    "url": "/static/js/main.e302f9c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07ac46d3fc9960d2447befecca78f23d",
+    "url": "/static/media/academia.07ac46d3.svg"
+  },
+  {
+    "revision": "8f9b0ffc1b348494728c2f593510e3de",
+    "url": "/static/media/contact.8f9b0ffc.svg"
+  },
+  {
+    "revision": "ce831940353900398476b775b414daeb",
+    "url": "/static/media/cs-course-02.ce831940.png"
+  },
+  {
+    "revision": "819bfc5d13bd7235da962be9f8c0f697",
+    "url": "/static/media/cs-course-03.819bfc5d.png"
+  },
+  {
+    "revision": "00ce433635609b3959e896d8fbc96f9b",
+    "url": "/static/media/cs-course-04.00ce4336.png"
+  },
+  {
+    "revision": "36e3430d7237539f39d0a07889643689",
+    "url": "/static/media/cs-course-05.36e3430d.png"
+  },
+  {
+    "revision": "bdad26df45e71b70aca118f1a44e92d7",
+    "url": "/static/media/cs-course-06.bdad26df.png"
+  },
+  {
+    "revision": "6d6b7c1b5353f909e75f7f9ead2c3e5e",
+    "url": "/static/media/cs-course-07.6d6b7c1b.png"
+  },
+  {
+    "revision": "03e33d4a1a6f61c1bac2f5b811765769",
+    "url": "/static/media/cs-course-08.03e33d4a.png"
+  },
+  {
+    "revision": "1bcf3e4b02ade94807308bfd4f035e2a",
+    "url": "/static/media/cs-course-09.1bcf3e4b.png"
+  },
+  {
+    "revision": "ea6f7e007f72734c2aa2cbd9f626896e",
+    "url": "/static/media/gray-blur.ea6f7e00.jpg"
+  },
+  {
+    "revision": "5f526aa414b003d5a09ade51a6eb4b89",
+    "url": "/static/media/home.5f526aa4.svg"
+  },
+  {
+    "revision": "98e3f30876d8d24fdeb5e35b5d0f6784",
+    "url": "/static/media/industry.98e3f308.svg"
+  },
+  {
+    "revision": "c605de61d2e96eaffdb44f08127680d4",
+    "url": "/static/media/projects.c605de61.svg"
+  },
+  {
+    "revision": "d6438bd260bd0ed0d6c30fcf5f5111a4",
+    "url": "/static/media/studio.d6438bd2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.01874efcf69901bbe571f9d251e78924.js"
 );
 
 self.addEventListener('message', (event) => {
